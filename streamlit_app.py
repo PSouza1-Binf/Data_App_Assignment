4572f0935ea6686d3eddf2e15c8fd45f768eb0de
PSouza1-Binf <77850219+PSouza1-Binf@users.noreply.github.com>
2025-03-19 10:56:41 -0400

    rounded metrics into tens place

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,17 +53,17 @@ sales_by_month_multi = multi_df.filter(items=['Sales']).groupby(pd.Grouper(freq=
 st.write("### (3) show a line chart of sales for the selected items in (2)")
 st.line_chart(sales_by_month_multi, y="Sales")
 st.write("### (4) show three metrics (https://docs.streamlit.io/library/api-reference/data/st.metric) for the selected items in (2): total sales, total profit, and overall profit margin (%)")
-sum_sales = sum(multi_df['Sales'])
-sum_all_sales = sum(df['Sales'])
-sum_profit = sum(multi_df['Profit'])
+sum_sales = round(sum(multi_df['Sales']), 2)
+sum_all_sales = round(sum(df['Sales']), 2)
+sum_profit = round(sum(multi_df['Profit']), 2)
 
 df["Profit_Margin"] = (df["Profit"] / df["Sales"]) * 100  # Calculate profit margin
 multi_df["Profit_Margin"] = (multi_df["Profit"] / multi_df["Sales"]) * 100
-avg_margin = multi_df["Profit_Margin"].mean()
-avg_margin_all = df["Profit_Margin"].mean()
-delta_margin = avg_margin-avg_margin_all
+avg_margin = round(multi_df["Profit_Margin"].mean(), 2)
+avg_margin_all = round(df["Profit_Margin"].mean(), 2)
+delta_margin = round(avg_margin - avg_margin_all, 2)
 
 st.metric(label="Total Sales", value= sum_sales)
 st.metric(label="Total Profit", value=sum_profit)
-st.metric(label="Profit Margin", value=avg_margin, delta= delta_margin)
+st.metric(label="Profit Margin", value=f"{avg_margin}%", delta=f"{delta_margin}%")
 st.write("### (5) use the delta option in the overall profit margin metric to show the difference between the overall average profit margin (all products across all categories)")
